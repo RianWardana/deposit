fac31d5dfa2040e4875ba4a4c3b07b5a5c085382
Rian Wardana <gmail@rianwardana.com>
2020-06-16 09:48:15 +0700

    Fix indentation
    Mencoba gunakan FirebaseUI tapi tak berhasil

--- src/app-auth.js
+++ src/app-auth.js
@@ -1,6 +1,11 @@
 import {PolymerElement, html} from '@polymer/polymer';
 import firebase from '@firebase/app';
 import '@firebase/auth';
+// please uninstall firebase and firebaseui from npm
+/* 
+16 Juni 2020
+Tidak bisa menggunakan FirebaseUI karena masalah Polymer 3 di ES6 import
+*/
 
 class appAuth extends PolymerElement {
   static get template() {
@@ -30,7 +35,6 @@ class appAuth extends PolymerElement {
             <paper-input label="E-mail address" value="{{email}}" maxlength="64"></paper-input>
             <paper-input label="Password" type="password" value="{{password}}" maxlength="32"></paper-input>
             <paper-button raised="" on-tap="_tapLogin">Log in</paper-button>
-            <!-- <paper-button raised on-tap="">Sign in with Google</paper-button> -->
         </div>
 
         <paper-toast id="toast"></paper-toast>
@@ -68,33 +72,33 @@ class appAuth extends PolymerElement {
   }
 
   ready() {
-      super.ready();
-      this.$.container.style.marginTop = (window.innerHeight - 280)/2 - 100 + 'px'
-      this.$.container.style.marginLeft = (window.innerWidth - 280)/2 + 'px'
+        super.ready();
+        this.$.container.style.marginTop = (window.innerHeight - 280)/2 - 100 + 'px'
+        this.$.container.style.marginLeft = (window.innerWidth - 280)/2 + 'px'
 
-      var config = {
-          apiKey: "AIzaSyCAmpmMEFT9nbMaTjI4YDqAa1H1zufc9r0",
-          authDomain: "deposit-ce46e.firebaseapp.com",
-          databaseURL: "https://deposit-ce46e.firebaseio.com",
-          storageBucket: "deposit-ce46e.appspot.com",
-          messagingSenderId: "713372742623"
-      }; 
-      firebase.initializeApp(config);
+        var config = {
+            apiKey: "AIzaSyCAmpmMEFT9nbMaTjI4YDqAa1H1zufc9r0",
+            authDomain: "deposit-ce46e.firebaseapp.com",
+            databaseURL: "https://deposit-ce46e.firebaseio.com",
+            storageBucket: "deposit-ce46e.appspot.com",
+            messagingSenderId: "713372742623"
+        }; 
+        firebase.initializeApp(config);
 
-      window.auth = firebase.auth();
+        window.auth = firebase.auth();
 
-      auth.onAuthStateChanged(firebaseUser => {
-          if (firebaseUser) {
-              this.uid = firebaseUser.uid;
-              this.loginStatus = 1;
-              this.email = "";
-              this.password = "";
-              this.$.toast.close();
-          }
-          else {
-              this.loginStatus = 0;
-          }
-      })
+        auth.onAuthStateChanged(firebaseUser => {
+            if (firebaseUser) {
+                this.uid = firebaseUser.uid;
+                this.loginStatus = 1;
+                this.email = "";
+                this.password = "";
+                this.$.toast.close();
+            }
+            else {
+                this.loginStatus = 0;
+            }
+        })
   }
 
   _triggerLogout() {
