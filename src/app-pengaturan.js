b1f0bc7fe6b1f132d080f25ac4d6149176ae03c0
Rian Wardana <gmail@rianwardana.com>
2020-06-17 14:16:50 +0700

    Create app-pengaturan.js

---
+++ src/app-pengaturan.js
@@ -0,0 +1,15 @@
+import {PolymerElement, html} from '@polymer/polymer';
+// import './pengaturan-data.js';
+
+class appPengaturan extends PolymerElement {
+  static get template() {
+    return html`
+    `;
+  }
+
+  ready() {
+      
+  }
+}
+
+customElements.define('app-pengaturan', appRingkasan);
