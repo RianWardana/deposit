b05977d9d96395d5ff39c342ce2f28ebd9c6671d
Rian Wardana <gmail@rianwardana.com>
2020-06-17 20:22:17 +0700

    use lit-element in tagihan-item

--- src/tagihan-item.js
+++ src/tagihan-item.js
@@ -1,9 +1,9 @@
-import {PolymerElement, html} from '@polymer/polymer';
+import {LitElement, html, css} from 'lit-element';
+import {styles} from './lit-styles.js';
 
-class tagihanItem extends PolymerElement {
-  static get template() {
-    return html`
-        <style include="iron-flex iron-flex-alignment shared-styles">  
+class tagihanItem extends LitElement {
+    static get styles() {
+        return [styles, css`
             a { 
                 text-decoration: none;
             }
@@ -12,51 +12,51 @@ class tagihanItem extends PolymerElement {
                 color: #FFAB00;
                 margin-left: 10px;
             }
-        </style>
+        `];
+    }
 
-        <paper-material>
-            <paper-ripple recenters=""></paper-ripple>
-            <div class="content">
-                <div class="horizontal layout">
-                    <span>[[nama]]</span> 
-                    <span class="flex"></span>
-                    <span>[[formatJumlah(jumlah)]]</span>
+    render() {
+        return html`
+            <paper-material>
+                <paper-ripple recenters=""></paper-ripple>
+                <div class="content">
+                    <div class="flex-space-between">
+                        <span>${this.nama}</span> 
+                        <span>${this.formatJumlah(this.jumlah)}</span>
+                    </div>
+                    <div class="flex-space-between">
+                        <span>${this.waktu}</span> 
+                        <a href="#">
+                            <span role="button" @click="${
+                                e => this._tapEdit(this.key, this.nama, this.jumlah)
+                            }">Edit</span>
+                        </a>
+                    </div>
                 </div>
-                <div class="horizontal layout">
-                    <span>[[waktu]]</span> 
-                    <span class="flex"></span>
-                    <a href="#"><span role="button" on-tap="_tapEdit" key="[[key]]" nama="[[nama]]" jumlah="[[jumlah]]">Edit</span></a>
-                </div>
-            </div>
-        </paper-material>
-`;
-  }
+            </paper-material>
+        `;
+    }
 
-  static get is() {
-      return 'tagihan-item';
-  }
+    static get properties() {
+        return {
+            key: String,
+            waktu: String,
+            nama: String,
+            jumlah: Number
+        }
+    }
 
-  static get properties() {
-      return {
-          key: String,
-          waktu: String,
-          nama: String,
-          jumlah: Number
-      }
-  }
+    formatJumlah(dataJumlah) {
+        var dataJumlahInt = parseInt(dataJumlah);
+        return "Rp" + dataJumlahInt.toLocaleString('id-ID');
+    }
 
-  formatJumlah(dataJumlah) {
-      var dataJumlahInt = parseInt(dataJumlah);
-      return "Rp" + dataJumlahInt.toLocaleString('id-ID');
-  }
-
-  // Selanjutnya gunakanlah this.dispatch atau apalah gitu, intinya dia bikin event yang bisa di-listen
-  _tapEdit(e) {
-      thisTagEdit.key = e.target.key;
-      thisTagEdit.nama = e.target.nama;
-      thisTagEdit.jumlah = e.target.jumlah;
-      thisTagEdit.triggerEdit = (thisTagEdit.triggerEdit ? false : true);
-  }
+    _tapEdit(a, b, c) {
+        thisTagEdit.key = a;
+        thisTagEdit.nama = b;
+        thisTagEdit.jumlah = c;
+        thisTagEdit.triggerEdit = (thisTagEdit.triggerEdit ? false : true);
+    }
 }
 
-customElements.define(tagihanItem.is, tagihanItem);
+customElements.define('tagihan-item', tagihanItem);
