6bf80a30bebeba4c466933a9634f811c80740ba1
Rian Wardana <gmail@rianwardana.com>
2020-06-19 21:52:40 +0700

    Convert to lit-element

--- src/rekening-list.js
+++ src/rekening-list.js
@@ -1,118 +1,76 @@
-import {PolymerElement, html} from '@polymer/polymer';
+import {LitElement, html, css} from 'lit-element';
+import {styles} from './lit-styles.js';
+import {until} from 'lit-html/directives/until.js';
 
-class rekeningList extends PolymerElement {
-  // nanti pakai ${ until(content, <spinner/>) }
-  static get template() {
-    return html`
-        <style include="iron-flex iron-flex-alignment shared-styles">
+class rekeningList extends LitElement {
+    
+    static get properties() {
+        return {
+            data: { 
+                type: Array,
+                notify: true,
+                observer: '_dataChanged'
+            },
+
+            saldo: {
+                type: Number,
+                value: 0
+            }
+        };
+    }
+    
+    static get styles() {
+        return [styles, css`
             paper-material#paper-material-saldo {
                 margin-top: 32px;
                 margin-bottom: 32px;
                 background: var(--app-primary-color);
                 color: white;
             }
-        </style>
-        
-        <div class="horizontal layout center-justified">
-            <paper-spinner id="spinner" active=""></paper-spinner>
-        </div>
 
-        <div class="narrow" id="list">
-            <paper-material id="paper-material-saldo">
-                <div class="horizontal layout">
-                    <span>Saldo di rekening</span>
-                    <span class="flex"></span>
-                    <span><b>Rp{{formatSaldo(saldo)}}</b></span>
-                </div>
-            </paper-material>
-            
-            <template is="dom-repeat" items="{{data}}" as="item">
-                <rekening-item waktu="{{item.waktu}}" nama="{{item.nama}}" jumlah="{{item.jumlah}}" jenis="{{item.jenis}}">
+            .spinnerContainer {
+                display: flex;
+                justify-content: center;
+            }
+        `];
+    }
+    
+    render() {
+        const items = new Promise(resolve => {
+            if (typeof this.data == 'undefined') return;
+            let load = this.data.map(item => html`
+                <rekening-item 
+                    waktu="${item.waktu}" 
+                    nama="${item.nama}" 
+                    jumlah="${item.jumlah}" 
+                    jenis="${item.jenis}">
                 </rekening-item>
-            </template>
-        </div>
-`;
-  }
+            `)
+            resolve(load);
+        });
 
-  static get is() {
-      return 'rekening-list';
-  }
+        const spinner = html`
+            <div class="spinnerContainer">
+                <paper-spinner active></paper-spinner>
+            </div>
+        `;
 
-  static get properties() {
-      return {
-          data: { 
-              type: Object,
-              notify: true,
-              observer: '_dataChanged'
-          },
+        return html`
+            <div class="narrow" id="list">
+                <paper-material id="paper-material-saldo" class="flexSpaceBetween">
+                    <span>Saldo di rekening</span>
+                    <span><b>Rp${this.formatSaldo(this.saldo)}</b></span>
+                </paper-material>
+                
+                ${until(items, spinner)}
+            </div>
+        `;
+    }
 
-          // persistedData: { 
-          //     type: Object,
-          //     notify: true,
-          //     observer: '_dataChanged'
-          // },
-
-          saldo: {
-              type: Number,
-              value: 0
-          },
-
-          daftarHari: {
-              type: Array,
-              value: [
-                  'Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'
-              ]
-          },
-
-          daftarBulan: {
-              type: Array,
-              value: [
-                  'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
-              ]
-          }
-      };
-  }
-
-  ready() {
-      super.ready();
-      this.dataChangedCount = 0;
-      this.animationFinishedCount = 0;
-
-      auth.onAuthStateChanged(firebaseUser => {
-          if (firebaseUser) {
-              this.dataChangedCount = 0;
-              this.animationFinishedCount = 0;
-              this.$.spinner.style.display = 'block';
-              this.$.list.style.display = 'none';
-          }
-          else {
-              this.dataChangedCount = -1;
-          }
-      })
-  }
-
-  _dataChanged() {
-      console.log("_dataChanged number " + this.dataChangedCount + " is called.");
-      // this.playAnimation('exitSpinner');
-      this.$.list.style.display = 'block';
-      this.$.spinner.style.display = 'none';
-      // this.playAnimation('entryList');
-      this.dataChangedCount++;
-  }
-
-  _animationFinished() {
-      if (this.animationFinishedCount == 0) {
-          this.$.spinner.style.display = 'none';
-          // this.$.list.style.display = 'block';
-          // this.playAnimation('entryList');
-          this.animationFinishedCount++;
-      }
-  }
-
-  formatSaldo(dataSaldo) {
-      if (isNaN(dataSaldo)) return 0;
-      else return parseInt(dataSaldo).toLocaleString('id-ID');
-  }
+    formatSaldo(dataSaldo) {
+        if (isNaN(dataSaldo)) return 0;
+        else return parseInt(dataSaldo).toLocaleString('id-ID');
+    }
 }
 
-customElements.define(rekeningList.is, rekeningList);
+customElements.define('rekening-list', rekeningList);
